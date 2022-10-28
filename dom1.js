c9c7a2bde94a0c51f3778924f5b2407ecb377d90
alka222 <alkabharadwaj222@gmail.com>
2022-10-28 16:37:12 +0530

    dom1 commit

---
+++ dom1.js
@@ -0,0 +1,29 @@
+// console.log(document);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.all[10].innerText);
+
+// document.all[10].innerText = "Hello";
+
+// console.log(document.all[10].innerText);
+
+// console.log(document.forms[0]);
+// console.log(document.images);
+
+//getElementByID
+
+// console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+
+// headerTitle.textContent = 'Hello';
+
+// console.log(headerTitle);
+headerTitle.style.borderBottom = 'solid 3px black';
+
+var h2title = document.getElementsByClassName('title');
+h2title[0].style.fontWeight = 'bold';
+h2title[0].style.color = 'green';
+
+
+
